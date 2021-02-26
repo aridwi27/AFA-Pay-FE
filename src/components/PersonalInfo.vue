d4dc1ff27f888c122c604bf6d412a2065d67b8db
cotbakheu <72972074+cotbakheu@users.noreply.github.com>
2021-02-26 09:47:25 +0700

    personal info and logout

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -1,27 +1,59 @@
 <template>
   <div>
-    <div class="card shadow-nm" style="border-radius:25px">
+    <div class="card shadow-nm" style="border-radius: 25px">
       <div class="card-body px-5">
         <h5 class="font-weight-bold mb-4">Personal Information</h5>
-        <p class="text-secondary w-50 mb-2">We got your personal information from the sign up proccess. If you want to make changes on your information, contact our support.</p>
+        <p class="text-secondary w-50 mb-2">
+          We got your personal information from the sign up proccess. If you
+          want to make changes on your information, contact our support.
+        </p>
         <form action="" class="my-5">
           <div class="form-group">
             <label class="text-secondary">First Name</label>
-            <input type="text" value="Robert" class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm">
+            <input
+              type="text"
+              v-model="detUser.first_name"
+              class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm"
+            />
           </div>
           <div class="form-group">
             <label class="text-secondary">Last name</label>
-            <input type="text" value="Chandler" class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm">
+            <input
+              type="text"
+              v-model="detUser.last_name"
+              class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm"
+            />
           </div>
           <div class="form-group">
             <label class="text-secondary">Verified Email</label>
-            <input type="email" value="pewdiepie1@gmail.com" class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm">
+            <input
+              type="email"
+              v-model="detUser.email"
+              class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm"
+            />
           </div>
           <div class="form-group">
             <label class="text-secondary">Phone Number</label>
-            <router-link to="/addphone" class=" float-right text-main"> Add Number</router-link>
-            <router-link to="/managephone" class=" float-right text-main">Manage  / </router-link>
-            <input type="text" value="+62 813-9387-7946" class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm">
+            <router-link to="/addphone" class="float-right text-main">
+              Add Number</router-link
+            >
+            <router-link to="/managephone" class="float-right text-main"
+              >Manage /
+            </router-link>
+            <input
+              readonly
+              v-if="detUser.handphone === '+62'"
+              type="text"
+              value="-"
+              class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm"
+            />
+            <input
+              readonly
+              v-else
+              type="text"
+              :value="detUser.handphone"
+              class="classname font-weight-bold form-control border-top-0 border-0 shadow-sm"
+            />
           </div>
         </form>
       </div>
@@ -30,15 +62,30 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 import { paymentMixin } from '../helpers/mixin'
 export default {
   mixins: [paymentMixin],
   data () {
     return {
-      amount: 0
+      amount: 0,
+      data: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone: ''
+      }
     }
   },
+  computed: {
+    ...mapGetters({
+      detUser: 'auth/detailUser'
+    })
+  },
   methods: {
+    ...mapActions({
+      actionsDetUser: 'auth/userDetail'
+    }),
     formatAmount () {
       this.amount = this.formatPrice(this.amount)
     }
@@ -47,5 +94,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="card border-0 h-100 shadow-nm" style="border-radius: 25px;">
+  <div class="card border-0 h-100 shadow-nm" style="border-radius: 25px">
     <div class="card-body px-0">
       <ul class="side-nav list-group" style="min-height: 60vh">
         <li @click="goto('/home')" class="list-group-item pl-4 side-nav-item">
-            <i class="fas fa-th-large ml-3 mr-4"></i>Dashboard
+          <i class="fas fa-th-large ml-3 mr-4"></i>Dashboard
         </li>
         <li @click="goto('/search')" class="list-group-item pl-4 side-nav-item">
-            <i class="fas fa-arrow-up ml-3 mr-4"></i>Transfer
+          <i class="fas fa-arrow-up ml-3 mr-4"></i>Transfer
         </li>
         <li @click="goto('/topup')" class="list-group-item pl-4 side-nav-item">
-            <i class="fas fa-plus ml-3 mr-4"></i>Top Up
+          <i class="fas fa-plus ml-3 mr-4"></i>Top Up
         </li>
-        <li @click="goto('/profile')" class="list-group-item pl-4 side-nav-item">
-            <i class="far fa-user ml-3 mr-4"></i>Profile
+        <li
+          @click="goto('/profile')"
+          class="list-group-item pl-4 side-nav-item"
+        >
+          <i class="far fa-user ml-3 mr-4"></i>Profile
         </li>
         <li @click="logout()" class="list-group-item pl-4 side-nav-item">
-            <i class="fas fa-sign-out-alt ml-3 mr-4"></i>Logout
+          <i class="fas fa-sign-out-alt ml-3 mr-4"></i>Logout
         </li>
       </ul>
     </div>
@@ -23,17 +26,25 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   data () {
     return {
     }
   },
   methods: {
+    ...mapActions({
+      onLogout: 'auth/logout'
+    }),
     goto (route) {
       this.$router.push(route)
     },
     logout () {
-      alert('Logout')
+      const check = confirm('Do you want to logout?')
+      if (check) {
+        this.onLogout()
+        this.$router.push('/')
+      }
     }
   }
 }
