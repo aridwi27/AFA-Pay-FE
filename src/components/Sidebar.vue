7baff1b0ad569e595aaf01f73baaa89d48bdd442
alifma <alifmaulana26@gmail.com>
2021-02-27 18:48:47 +0700

    Sidebar Active Highlight

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,28 +5,28 @@
         <li
           @click="goto('home')"
           style="cursor: pointer"
-          class="list-group-item pl-4 side-nav-item"
+          :class="`list-group-item pl-4 side-nav-item ${$route.name === 'home' ? 'onroute ' : ''}`"
         >
           <i class="fas fa-th-large ml-3 mr-4"></i>Dashboard
         </li>
         <li
           @click="goto('search')"
           style="cursor: pointer"
-          class="list-group-item pl-4 side-nav-item"
+          :class="`list-group-item pl-4 side-nav-item ${$route.name === 'search' ? 'onroute ' : ''}`"
         >
           <i class="fas fa-arrow-up ml-3 mr-4"></i>Transfer
         </li>
         <li
           @click="goto('topup')"
           style="cursor: pointer"
-          class="list-group-item pl-4 side-nav-item"
+          :class="`list-group-item pl-4 side-nav-item ${$route.name === 'topup' ? 'onroute ' : ''}`"
         >
           <i class="fas fa-plus ml-3 mr-4"></i>Top Up
         </li>
         <li
           @click="goto('profile')"
           style="cursor: pointer"
-          class="list-group-item pl-4 side-nav-item"
+          :class="`list-group-item pl-4 side-nav-item ${$route.name === 'profile' ? 'onroute ' : ''}`"
         >
           <i class="far fa-user ml-3 mr-4"></i>Profile
         </li>
